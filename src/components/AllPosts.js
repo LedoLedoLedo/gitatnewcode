4acb8a6e02c827f78788651f5cb10ad4e8305d5f
Alejandro Fernandez-Sarasua <alfea1983@gmail.com>
2022-08-05 08:18:23 -0400

    removed unused photos, styled post page

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -27,27 +27,39 @@ const AllPosts = () => {
 
   return (
     <div className="allPostsPage">
-      <h2>Gig Postings</h2>
+      <h1>Gig Postings</h1>
       <h3>{status}</h3>
       {posts.map((post) => {
         return (
           <div className="postCard">
-            <h4>
-              {/* Created by:{post.creatorId.username} */}
-              <Link to={`/users/${post.creatorId._id}`}>
-                {post.creatorId.username}
-              </Link>
-            </h4>
-            <p>{post.content}</p>
-            <button
-              className="delete"
-              onClick={() => {
-                deletePost(post._id);
-              }}
-            >
-              {" "}
-              Delete{" "}
-            </button>
+            <section className="postCardLeft">
+              <div className="profileIcon"></div>
+            </section>
+            <section className="postCardCenter">
+              <section className="postCardContent">
+                <span className="postCardUserName">
+                  {/* Created by:{post.creatorId.username} */}
+                  <Link to={`/users/${post.creatorId._id}`}>
+                    {post.creatorId.username}
+                  </Link>
+                </span>
+                <span>{post.content}</span>
+              </section>
+              <section className="postCardActions">
+                <button
+                  className="delete"
+                  onClick={() => {
+                    deletePost(post._id);
+                  }}
+                >
+                  {" "}
+                  Delete{" "}
+                </button>
+              </section>
+            </section>
+            <section className="postCardRight">
+              <span>30 min</span>
+            </section>
           </div>
         );
       })}
